c6e6372f0c042766e45307aec7522424b6e499d7
Rakesh Kumar <35292864+rakesh0079@users.noreply.github.com>
2023-02-05 20:02:30 +0530

    Update jenkins3.groovy

--- jenkins3.groovy
+++ jenkins3.groovy
@@ -11,23 +11,23 @@ pipeline {
 
         stage ('Compile Stage') {
 steps {
-withMaven(maven : 'apache-maven-3.6.8') {
+
 bat'mvn clean compile'
-}
+
 }
 }
 stage ('Testing Stage') {
 steps {
-withMaven(maven : 'apache-maven-3.6.8') {
+
 bat'mvn test'
-}
+
 }
 }
 stage ('Install Stage') {
 steps {
-withMaven(maven : 'apache-maven-3.6.8') {
+
 bat'mvn install'
-}
+
 }
 }
   }

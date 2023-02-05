ca6b5e4064feb5f8fdbea28a98d47a245407c8ec
Rakesh Kumar <35292864+rakesh0079@users.noreply.github.com>
2023-02-05 20:22:02 +0530

    Update jenkins3.groovy

--- jenkins3.groovy
+++ jenkins3.groovy
@@ -40,3 +40,50 @@ success {
 }
   }
 }
+pipeline {
+   tools { 
+        maven 'localMaven' 
+        jdk 'JDK11' 
+    }
+    agent any
+
+
+
+    stages {
+
+        stage ('Compile Stage') {
+steps {
+
+bat'mvn clean compile'
+
+}
+}
+stage ('Testing Stage') {
+steps {
+
+bat'mvn test'
+
+}
+}
+stage ('Install Stage') {
+steps {
+
+bat'mvn install'
+
+}
+post {
+success {
+    echo 'Archieving  rtifact ..'
+    archiveArtifacts artifacts: '**/*.war'
+
+}
+
+}
+}
+stage ('Deployment Stage') {
+ echo 'Deployment  Start ..'
+ build job: 'StagingDeploymentArtifactPipeline'
+}
+  
+}
+}
